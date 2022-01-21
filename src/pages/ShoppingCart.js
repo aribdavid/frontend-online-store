53d856be5c63604a519395b3f4d12f2a0010a5a6
aribdavid <aryehbdavid@gmail.com>
2022-01-21 12:33:23 -0300

    requisito 8 preview

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class ShoppingCart extends React.Component {
-  render() {
-    return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-    );
-  }
+  removeDuplicates = (array) => array.filter((elem, index) => array.indexOf(elem) === index);
+
+   itemQuantity = (arr, val) => arr.reduce((counter, item) => (item === val ? counter + 1 : counter), 0);
+
+   render() {
+     return (
+       <div>
+         Meus Produtos
+         {this.removeDuplicates(this.props.cartProducts).map((item) => (
+           <div key={ item }>
+             <p data-testid="shopping-cart-product-name">{item}</p>
+             <p data-testid="shopping-cart-product-quantity">
+               Quantidade:
+               {' '}
+               {this.itemQuantity(this.props.cartProducts, item)}
+             </p>
+           </div>
+         )) }
+       </div>
+     );
+   }
 }
 
 export default ShoppingCart;
